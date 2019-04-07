84f3fce9199f9f7b830f13b8157d354af460cd2f
Jason Michaels <jason.michaels721@gmail.com>
2019-04-07 14:49:29 +0000

    correctly using reactive rxjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import { Observable, noop} from 'rxjs';
+import { Observable, noop } from 'rxjs';
 import { map, filter} from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -21,39 +21,27 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
-      const http$ = createHttpObservable('/api/courses');
+      const http$: Observable<Course[]> = createHttpObservable('/api/courses');
 
       const courses$ = http$
         .pipe(
-          map(res => Object.values(res["payload"]))
+          map(res => res["payload"])
         );
 
 
 
-      this.beginnerCourses$ = http$
-        .pipe(
-            map(courses => courses)
-                    ,filter(course => course == '')
-        );
+      this.beginnerCourses$ = courses$
+         .pipe(
+             map((courses) => courses.
+             filter((course) => course["category"] == 'BEGINNER'))
+         );
 
-        console.log(this.advancedCourses$)
 
-     /* this.advancedCourses$ = http$
-        .pipe(
-            map(courses => courses
-                .filter(course => course.category == 'ADVANCED'))
-        ); */
-
-      courses$.subscribe(
-        courses => {
-
-            console.log(courses);
-
-        },
-        noop,
-        () => console.log('completed')
-      );
-
+      this.advancedCourses$ = courses$
+         .pipe(
+             map((courses) => courses.
+             filter((course) => course["category"] == 'ADVANCED'))
+         );
 
 
     }
